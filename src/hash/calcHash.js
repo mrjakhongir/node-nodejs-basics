e9a1bef2f6b9092a8dbb6c6a85926d28e72502af
Jahongir <nusratovjahongir@gmail.com>
2025-04-28 12:52:07 +0500

    feat: complete hash task.

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import { join } from "path";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = join("files", "fileToCalculateHashFor.txt");
+  const hash = crypto.createHash("sha256");
+  const stream = fs.createReadStream(filePath);
+
+  stream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on("end", () => {
+    const result = hash.digest("hex");
+    console.log(`SHA256 hash of the file is: ${result}`);
+  });
+
+  stream.on("error", (err) => {
+    console.error(`Error reading the file: ${err.message}`);
+  });
 };
 
 await calculateHash();
