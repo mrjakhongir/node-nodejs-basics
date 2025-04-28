fd3b8cca46a8b0e9fc483dae50970fca403efd23
Jahongir <nusratovjahongir@gmail.com>
2025-04-28 18:27:18 +0500

    feat: complete Worker Threads task.

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+// main.js
+import { cpus } from "os";
+import { Worker } from "worker_threads";
+import { join } from "path";
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCPUs = cpus().length;
+  const workers = [];
+
+  for (let i = 0; i < numCPUs; i++) {
+    workers.push(
+      new Promise((resolve) => {
+        const worker = new Worker(
+          join(new URL(".", import.meta.url).pathname, "worker.js")
+        );
+
+        worker.on("message", (data) => {
+          resolve(data);
+        });
+
+        worker.on("error", () => {
+          resolve({ status: "error", data: null });
+        });
+
+        worker.postMessage(10 + i);
+      })
+    );
+  }
+
+  const settledResults = await Promise.all(workers);
+  console.log(settledResults);
 };
 
 await performCalculations();
