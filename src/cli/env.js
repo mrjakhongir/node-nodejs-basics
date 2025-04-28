18fe2c35668673bb4a56510ff871e3f117ad6b53
Jahongir <nusratovjahongir@gmail.com>
2025-04-28 12:12:01 +0500

    feat: complete cli tasks.

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssVars = Object.entries(process.env)
+    .map(([key, value]) => `RSS_${key}=${value}`)
+    .join("; \n");
+
+  console.log(rssVars);
 };
 
 parseEnv();
